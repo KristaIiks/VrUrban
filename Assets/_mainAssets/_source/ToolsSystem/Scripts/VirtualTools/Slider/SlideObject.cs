5b07d50df59601a5ad1d8b6be9ab81670ce9587a
KristaIiks <88441151+KristaIiks@users.noreply.github.com>
2025-02-21 11:22:03 +0300

    Add slider + fix room

---
+++ Assets/_mainAssets/_source/ToolsSystem/Scripts/VirtualTools/Slider/SlideObject.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace ToolsSystem
+{
+	public class SlideObject : MonoBehaviour
+	{
+		[SerializeField] private GameObject FirstObject;
+		[SerializeField] private GameObject SecondObject;
+		
+		public void Select(bool first)
+		{
+			FirstObject.SetActive(first);
+			SecondObject.SetActive(first);
+		}
+		
+		public void Reset()
+		{
+			if (Random.Range(0, 100) <= 50)
+			{
+				FirstObject.SetActive(true);
+				SecondObject.SetActive(false);
+			}
+			else
+			{
+				FirstObject.SetActive(false);
+				SecondObject.SetActive(true);
+			}
+		}
+	}
+}
---
+++ Assets/_mainAssets/_source/ToolsSystem/Scripts/VirtualTools/Slider/SlideVariants.cs
@@ -0,0 +1,50 @@
+using System;
+using StudySystem;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ToolsSystem
+{
+	public sealed class SlideVariants : MonoBehaviour, IStudyObject
+	{
+		[SerializeField] private GameObject Window;
+		[SerializeField] private Slider PercentSlider;
+		[SerializeField] private SlideObject[] Objects;
+		
+		private void Awake()
+		{
+			Restart(false);
+			PercentSlider.onValueChanged.AddListener(UpdateModels);
+		}
+		
+		private void UpdateModels(float value)
+		{
+			float percent = value / PercentSlider.maxValue;
+			
+			for (int i = 0; i < Objects.Length; i++)
+			{
+				Objects[i].Select(i / Objects.Length <= percent);
+			}
+		}
+		
+		public void StartDefaultStudy(Action OnComplete)
+		{
+			Restart(true);
+		}
+		
+		public void Skip()
+		{
+			UpdateModels(PercentSlider.maxValue);
+		}
+		
+		public void Restart(bool canContinue = true)
+		{
+			Window.SetActive(canContinue);
+			
+			foreach (SlideObject obj in Objects)
+			{
+				obj.Reset();
+			}
+		}
+	}
+}
